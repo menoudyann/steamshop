cfdb942ce3781084b164c203da5a6986e3c802ba
menoudyann <Yann.MENOUD@cpnv.ch>
2023-01-09 08:08:32 +0100

    WIP

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,59 +1,58 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-@section('title', 'Our Gaming Catalog')
-
-@section('content')
-
-<div class="gaming-library">
-    <div class="col-lg-12">
-        <div class="heading-section">
-            <h4><em>Your Gaming</em> Catalog</h4>
-        </div>
-        <?php foreach ($games as $game) : ?>
-            <div class="item">
-                <ul>
-                    <li><img src="{{asset('images/game-03.jpg')}}" alt="" class="templatemo-item"></li>
-
-                    <li>
-                        <h4><?= $game->name ?></h4>
-                    </li>
-                    <li>
-                        <h4><?= $game->released_at ?></h4>
-                    </li>
-                    <li>
-                        <h4><?= $game->price ?> CHF</h4>
-                    </li>
-                    <li>
-                        <form action="{{ route('games.show', $game) }}" method="GET">
-                            @csrf
-                            <div class="main-button">
-                                <button class="btn btn-success">Buy</button>
-                            </div>
-                        </form>
-                    </li>
-                    <li>
-                        @if (Auth::check())
-                        <form action="{{ route('games.destroy', $game) }}" method="POST">
-                            @method('DELETE')
-                            @csrf
-                            <div class="main-button">
-                                <button class="btn btn-danger" style="height: 40px; width: 40px"><i class="fa fa-trash"></i></button>
-                            </div>
-                        </form>
-                        @endif
-                    </li>
-                </ul>
-            </div>
-        <?php endforeach; ?>
-        @if (Auth::check())
-        <div class="main-button">
-            <a href="{{ route('games.create') }}">Add a new game</a>
-        </div>
-        @endif
+    <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md mx-10">
+        <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Release at</th>
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">price</th>
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+                <?php foreach ($games as $game) : ?>
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-6 py-4"><img src=" {{asset('images/game-03.jpg')}}" />
+                        </td>
+                        <td class="px-6 py-4">
+                            <h4><?= $game->name ?></h4>
+                        </td>
+                        <td class="px-6 py-4">
+                            <h4><?= $game->released_at ?></h4>
+                        </td>
+                        <td class="px-6 py-4">
+                            <h4><?= $game->price ?> CHF</h4>
+                        </td>
+                        <td class="px-6 py-4">
+                            @if (Auth::check())
+                            <form action="{{ route('games.destroy', $game) }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <div class="main-button">
+                                    <button class="btn btn-danger" style="height: 40px; width: 40px"><i class="fa fa-trash"></i></button>
+                                </div>
+                            </form>
+                            @endif
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                @if (Auth::check())
+                <div class="main-button">
+                    <a href="{{ route('games.create') }}">Add a new game</a>
+                </div>
+                @endif
+            </tbody>
+        </table>
     </div>
-    <div class="col-lg-12">
-        <div class="main-button">
-        </div>
     </div>
-</div>
-@endsection
+    </div>
+    </div>
+    </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('games.index');
+    return view('auth.register');
 });
 
-Route::get('/dashboard', function () {
-    return view('games.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::resource('games', GameController::class);
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

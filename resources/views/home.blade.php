f8096649d96c1ca11ee25dbd6b17c37701cdd293
menoudyann <yann.menoud@cpnv.ch>
2023-01-17 14:09:18 +0100

    create migration to add balance field in table users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,37 +7,43 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="bg-white ">
-                        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                            <div class="sm:text-left">
-                                <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Lastest release</p>
-                            </div>
-                            <div class="mt-20 max-w-lg sm:mx-auto md:max-w-none">
-                                <div class="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
-                                    @foreach ($games as $game)
-                                    <div class="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
-                                        <div class="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
-                                        <img class="rounded-lg" src=" {{asset('images/game-03.jpg')}}" />
+            <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md mx-10 mt-10">
+                <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">Image</th>
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">Release at</th>
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">Price</th>
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900" align="right"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+                        @foreach ($games as $game) 
+                            <tr class="hover:bg-gray-50">
+                                <td class="px-6 py-4"><img class="rounded-lg" src=" {{asset('images/game-03.jpg')}}" />
+                                </td>
+                                <td class="px-6 py-4">
+                                    <h4><?= $game->name ?></h4>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <h4><?= $game->released_at ?></h4>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <h4><?= $game->price ?> CHF</h4>
+                                </td>
+                                <td class="px-6 py-4" align="right">
+                                    <form action="{{ route('games.buy', $game) }}" method="POST">
+                                        @csrf
+                                        <div>
+                                        <button type="submit" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Buy</button>
                                         </div>
-                                        <div class="sm:min-w-0 sm:flex-1">
-                                            <p class="text-lg font-semibold leading-8 text-gray-900">{{$game->name}}</p>
-                                            <p class="mt-2 text-base leading-7 text-gray-600">{{$game->description}}</p>
-                                            <form action="{{ route('games.destroy', $game) }}" method="POST" class="my-4">
-                                                @csrf
-                                                <div>
-                                                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Buy</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
